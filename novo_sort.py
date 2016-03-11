fbf62dc6b9a1794b07b5189279096b7d4c03ea3b
zipho <zipho.m@gmail.com>
2016-03-11 09:30:10 +0200

    Cleaning up the the novo_sort tool

--- novo_sort.py
+++ novo_sort.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
 import argparse
 from subprocess import check_call, CalledProcessError
-from json import load, dump, dumps
-from os import environ, mkdir, makedirs, path
-from os.path import isdir, exists
 import shlex
 import sys
 import logging
 log = logging.getLogger( __name__ )
 
 def novo_sort( bam_filename ):
-    #novosort -c 8 -m 8G -s-f $i > $i".sorted"; done
     cmdline_str = "novosort -c 8 -m 8G -s -f {}".format( bam_filename )
     cmdline = newSplit(cmdline_str)
     try:
         check_call(cmdline)
     except CalledProcessError:
-        print("Error running the nova-align", file=sys.stderr)
+        print("Error running the nova-sort", file=sys.stderr)
 
 def newSplit(value):
     lex = shlex.shlex(value)
@@ -28,8 +23,7 @@ def newSplit(value):
     return list(lex)
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate a BAM file from the Novo Align tool")
-    parser.add_argument('output_filename')
+    parser = argparse.ArgumentParser(description="Re-sorting aligned files by read position")
     parser.add_argument('--bam_filename')
     args = parser.parse_args()
    
